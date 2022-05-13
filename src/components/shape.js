f593c155ed200396131c676f8d563f0e1390b6f5
Tejareddy22 <98025617+Tejareddy22@users.noreply.github.com>
2022-05-13 18:27:49 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
 // declare class
 
 // export class using module.exports
+class Shape{
+  constructor(){
+    this.color="black";
+  }
+
+  drawShape(){
+
+  }
+
+  calculateArea(){
+
+  }
+
+}
+// export class using module.exports
+
+module.exports =Shape;
